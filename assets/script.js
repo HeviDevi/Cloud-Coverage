ea0bfd68f3d97007ed193dffb4fa9beaa9e48c0c
HeviDevi <devon.r.ross@gmail.com>
2024-05-14 16:55:20 -0400

    The data is now being applied to the html elements, I will try to implement a loop to make the code smaller & then tackle adding searches to session or local storage

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,53 @@
 let searchEL = document.getElementById('search-btn');
 let capitolEL = document.querySelectorAll('.capitol-btn');
-// let chosenCity = 'London';
 
-displayWeatherData = (data) => {
+searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+
+
+displayWeatherData = (data) => { 
+document.getElementById('city-name').innerText = data.city.name;
+document.getElementById('date0').innerText = data.list[0].dt_txt.split(' ')[0];
+document.getElementById('currentTemp').innerText = 'Current Temperature is ' + data.list[0].main.temp.toFixed(0) + ' °F';
+document.getElementById('minTemp0').innerText = 'Low of ' + data.list[0].main.temp_min.toFixed(0) + ' °F';
+document.getElementById('maxTemp0').innerText = 'High of ' + data.list[0].main.temp_max.toFixed(0) + ' °F';
+document.getElementById('humidity0').innerText = 'Humidity: ' + data.list[0].main.humidity + '%';
+document.getElementById('wind0').innerText = 'Wind Speed: ' + data.list[0].wind.speed + ' mph';
+document.getElementById('icon0').src = 'https://openweathermap.org/img/wn/' + data.list[0].weather[0].icon + '@2x.png';
+
+document.getElementById('date1').innerText = data.list[8].dt_txt.split(' ')[0];
+document.getElementById('minTemp1').innerText = 'Low of ' + data.list[8].main.temp_min.toFixed(0) + ' °F';
+document.getElementById('maxTemp1').innerText = 'High of ' + data.list[8].main.temp_max.toFixed(0) + ' °F';
+document.getElementById('humidity1').innerText = 'Humidity: ' + data.list[8].main.humidity + '%';
+document.getElementById('wind1').innerText = 'Wind Speed: ' + data.list[8].wind.speed + ' mph';
+document.getElementById('icon1').src = 'https://openweathermap.org/img/wn/' + data.list[8].weather[0].icon + '.png';
+
+document.getElementById('date2').innerText = data.list[16].dt_txt.split(' ')[0];
+document.getElementById('minTemp2').innerText = 'Low of ' + data.list[16].main.temp_min.toFixed(0) + ' °F';
+document.getElementById('maxTemp2').innerText = 'High of ' + data.list[16].main.temp_max.toFixed(0) + ' °F';
+document.getElementById('humidity2').innerText = 'Humidity: ' + data.list[16].main.humidity + '%';
+document.getElementById('wind2').innerText = 'Wind Speed: ' + data.list[16].wind.speed + ' mph';
+document.getElementById('icon2').src = 'https://openweathermap.org/img/wn/' + data.list[16].weather[0].icon + '.png';
+
+document.getElementById('date3').innerText = data.list[24].dt_txt.split(' ')[0];
+document.getElementById('minTemp3').innerText = 'Low of ' + data.list[24].main.temp_min.toFixed(0) + ' °F';
+document.getElementById('maxTemp3').innerText = 'High of ' + data.list[24].main.temp_max.toFixed(0) + ' °F';
+document.getElementById('humidity3').innerText = 'Humidity: ' + data.list[24].main.humidity + '%';
+document.getElementById('wind3').innerText = 'Wind Speed: ' + data.list[24].wind.speed + ' mph';
+document.getElementById('icon3').src = 'https://openweathermap.org/img/wn/' + data.list[24].weather[0].icon + '.png';
+
+document.getElementById('date4').innerText = data.list[32].dt_txt.split(' ')[0];
+document.getElementById('minTemp4').innerText = 'Low of ' + data.list[32].main.temp_min.toFixed(0) + ' °F';
+document.getElementById('maxTemp4').innerText = 'High of ' + data.list[32].main.temp_max.toFixed(0) + ' °F';
+document.getElementById('humidity4').innerText ='Humidity: ' + data.list[32].main.humidity + '%';
+document.getElementById('wind4').innerText = 'Wind Speed: ' + data.list[32].wind.speed + ' mph';
+document.getElementById('icon4').src = 'https://openweathermap.org/img/wn/' + data.list[32].weather[0].icon + '.png';
+
+document.getElementById('date5').innerText = data.list[39].dt_txt.split(' ')[0];
+document.getElementById('minTemp5').innerText = 'Low of ' + data.list[39].main.temp_min.toFixed(0) + ' °F';
+document.getElementById('maxTemp5').innerText = 'High of ' + data.list[39].main.temp_max.toFixed(0) + ' °F';
+document.getElementById('humidity5').innerText = 'Humidity: ' + data.list[39].main.humidity + '%';
+document.getElementById('wind5').innerText = 'Wind Speed: ' + data.list[39].wind.speed + ' mph';
+document.getElementById('icon5').src = 'https://openweathermap.org/img/wn/' + data.list[39].weather[0].icon + '.png';
 
 };
 
@@ -10,7 +55,7 @@ displayWeatherData = (data) => {
 
 function getWeatherData(chosenCity) {
     fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?q=${chosenCity}&appid=28d152345e91987cbfc03a794bbf7131`
+        `https://api.openweathermap.org/data/2.5/forecast?q=${chosenCity}&appid=28d152345e91987cbfc03a794bbf7131&units=imperial`
     ) .then(function(response) {
         return response.json();
     })
@@ -43,38 +88,3 @@ document.addEventListener('DOMContentLoaded', function(){
     getWeatherData(chosenCity);
 });
 
-// function getWeatherData(lat, lon){
-// fetch(
-//     'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=28d152345e91987cbfc03a794bbf7131'
-// )
-// .then(function(response){ 
-//     return response.json()
-// })
-// .then(function(data){
-//     console.log(data)
-//     console.log('Done')
-//     displayWeatherData();
-// })
-// }
-
-
-// function getLatLon(){
-//     fetch(
-//     'https://api.openweathermap.org/geo/1.0/direct?q=' + chosenCity + '&limit=1&appid=28d152345e91987cbfc03a794bbf7131'
-// )
-// .then(function(response){
-//     return response.json()
-// })
-
-// .then(function(data){
-//     console.log(parseFloat(data[0].lat.toFixed(2)));
-//     console.log(parseFloat(data[0].lon.toFixed(2)));
-//     console.log(data)
-
-//     let lat = parseFloat(data[0].lat.toFixed(2));
-//     let lon = parseFloat(data[0].lon.toFixed(2));
-//     getWeatherData(lat, lon);
-// })
-// };
-
-// getLatLon();
